df0c75e7d31562fc956c4897b756101fbfc91bdf
antti4 <91421038+antti4@users.noreply.github.com>
2023-05-11 09:58:53 +0300

    initial commit

---
+++ Mobile_project_main/Mobile_project_main/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Mobile_project_main
+//
+//  Created by Antti Hokkinen on 11.5.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+
---
+++ Mobile_project_main/Mobile_project_main/Mobile_project_mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_project_mainApp.swift
+//  Mobile_project_main
+//
+//  Created by Antti Hokkinen on 11.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_project_mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
